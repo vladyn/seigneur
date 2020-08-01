3551ddf39397ff7f390d3234854bcb38c8bb373e
Vladimir Varbanov <vlado@vlado1.com>
2020-08-01 17:24:59 +0300

    refactoring

--- modules/chat-renderer.js
+++ modules/chat-renderer.js
@@ -2,6 +2,11 @@ const electron = require('electron');
 
 const ipc = electron.ipcRenderer;
 
-module.exports = (function() {
-  document.getElementById('hello').addEventListener('click', _ => ipc.send('start-timer'))
+module.exports = (() => {
+  const helloTrigger = document.getElementById('hello')
+  const startTimer = _ => {
+    ipc.send('start-timer')
+    helloTrigger.removeEventListener('click', startTimer)
+  }
+  helloTrigger.addEventListener('click', startTimer)
 })();
